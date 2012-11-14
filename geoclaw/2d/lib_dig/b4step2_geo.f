973ef0568f7fedb65ae648006a2e9e908aa432d4
David George <dave.jorge@gmail.com>
2012-11-14 13:03:10 -0800

    fixed bug in b4step2 b/(h+b), not b/h+b

--- geoclaw/2d/lib_dig/b4step2_geo.f
+++ geoclaw/2d/lib_dig/b4step2_geo.f
@@ -71,7 +71,7 @@ c=============mobilization =============================================
                if (bed_normal.eq.1) then
                   theta=aux(i,j,i_theta)
                   p_ratioij = max(0.0,init_p_ratio
-     &               - aux(i,j,1)/q(i,j,1)+aux(i,j,1))
+     &               - aux(i,j,1)/(q(i,j,1)+aux(i,j,1)))
                endif
                gmod = grav*dcos(theta)
 c               if ((q(i,j,2)**2 + q(i,j,3)**2).gt.1.d-16) cycle
@@ -94,7 +94,7 @@ c     &            rho_f*gmod*q(i,j,1)
                if (bed_normal.eq.1) then
                   theta=aux(i,j,i_theta)
                   p_ratioij = max(0.0,init_p_ratio
-     &               - aux(i,j,1)/q(i,j,1)+aux(i,j,1))
+     &               - aux(i,j,1)/(q(i,j,1)+aux(i,j,1)))
                endif
                gmod = grav*dcos(theta)
 c               if ((q(i,j,2)**2 + q(i,j,3)**2).gt.1.d-16) cycle
