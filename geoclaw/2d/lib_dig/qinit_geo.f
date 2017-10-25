af75d0711f42205a41e972fdc2dfea6a6e665354
David George <dave.jorge@gmail.com>
2017-10-25 13:21:39 -0700

    na

--- geoclaw/2d/lib_dig/qinit_geo.f
+++ geoclaw/2d/lib_dig/qinit_geo.f
@@ -99,9 +99,11 @@ c
       initm = 0
       initchi = 0
       do mf =1,mqinitfiles
+         if (iqinit(mf).eq.2) initu=1
+         if (iqinit(mf).eq.3) initv = 1
          if (iqinit(mf).eq.4) initm = 1
-         if (iqinit(mf).eq.6) initchi = 1
          if (iqinit(mf).eq.5) initpv = 1
+         if (iqinit(mf).eq.6) initchi = 1
       enddo
 
       do i=1-mbc,mx+mbc
@@ -116,10 +118,14 @@ c
                else
                   q(i,j,6) = q(i,j,1)*q(i,j,6)
                endif
-               if (initpv.eq.0) then
-              
-               else
-                  q(i,j,5) = q(i,j,1)*q(i,j,5)
+               if (initu.eq.1) then
+                  q(i,j,2) = q(i,j,1)*q(i,j,2)   
+               endif
+               if (initv.eq.1) then
+                  q(i,j,3) = q(i,j,1)*q(i,j,3)   
+               endif
+               if (initpv.eq.1) then
+                  q(i,j,5) = q(i,j,1)*q(i,j,5)   
                endif
                if (q(i,j,1).le.drytolerance) then
                   do m = 1,meqn
