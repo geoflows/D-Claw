8ffa6eaf90b549633716700e036e9f218333181e
George <dgeorge@igswwawgmc166.gs.doi.net>
2017-12-06 14:16:22 -0800

    gaugelocations

--- matlabgeo/plotgaugelocations.m
+++ matlabgeo/plotgaugelocations.m
@@ -1,12 +1,18 @@
 fid = fopen('setgauges.data');
-mgauges=fscanf(fid,'%g',[1]);
-gaugelocs = fscanf(fid,'%g',[3,inf])';
+for i=1:6
+    fgetl(fid);
+end
+[mgauges,count]=fscanf(fid,'%g',[1]);
+junk = fscanf(fid,'%s',2);
+
+[gaugelocs,count2] = fscanf(fid,'%g',[5,inf]);
+gaugelocs=gaugelocs';
 status = fclose(fid);
 
 hold on
 for i=1:mgauges
-  plot3(gaugelocs(i,2),gaugelocs(i,3),100,'o','MarkerSize',30)
-  plot3(gaugelocs(i,2),gaugelocs(i,3),100,'+','MarkerSize',25)
-  text(gaugelocs(i,2),gaugelocs(i,3),100,num2str(gaugelocs(i,1)),'FontSize',20)
+  plot3(gaugelocs(i,2),gaugelocs(i,3),9000,'o','MarkerSize',30)
+  plot3(gaugelocs(i,2),gaugelocs(i,3),9000,'+','MarkerSize',25)
+  text(gaugelocs(i,2),gaugelocs(i,3),9000,num2str(gaugelocs(i,1)),'FontSize',20)
   end
 hold off
