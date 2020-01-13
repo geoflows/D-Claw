943794b2e81b1b82dd969b5c424b79b1475d04d1
David George <dgeorge@usgs.gov>
2020-01-13 14:13:01 -0800

    python dclaw tools edit

--- python/dclaw/gaugedata.py
+++ python/dclaw/gaugedata.py
@@ -18,7 +18,8 @@ gaugedata
 
 """
 
-from pylab import *
+from pylab import * #I regret this import...need to clean-up at some point
+import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as pyplot
 import os
